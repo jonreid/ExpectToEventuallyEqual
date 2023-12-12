623123df75db051d1dbc315e2bf8ee18d005658a
Jon Reid <jon@qualitycoding.org>
2023-12-11 21:59:10 -0800

    t - attempt ubuntu fix by removing @MainActor

--- Tests/ExpectToEventuallyEqualTests/ExpectToEventuallyEqualTests.swift
+++ Tests/ExpectToEventuallyEqualTests/ExpectToEventuallyEqualTests.swift
@@ -1,7 +1,6 @@
 @testable import ExpectToEventuallyEqual
 import XCTest
 
-@MainActor
 final class ExpectToEventuallyEqualTests: XCTestCase {
     func test_immediateMatch() throws {
         try expectToEventuallyEqual(actual: { "abc" }, expected: "abc")
