d2b888d26744c32f9cb1af862d3e1f5a957d6ce5
Jon Reid <jon@qualitycoding.org>
2023-12-18 13:16:28 -0800

    t - directly test message for string, for int

--- Tests/ExpectToEventuallyEqualTests/ExpectToEventuallyEqualTests.swift
+++ Tests/ExpectToEventuallyEqualTests/ExpectToEventuallyEqualTests.swift
@@ -15,7 +15,7 @@ final class ExpectToEventuallyEqualTests: XCTestCase {
         )
     }
 
-    func test_failureMessage() throws {
+    func test_failure() throws {
         let changeling = Changeling(beforeChange: "nope")
         let failSpy = FailSpy()
 
@@ -30,6 +30,16 @@ final class ExpectToEventuallyEqualTests: XCTestCase {
         XCTAssertTrue(failSpy.message.hasSuffix(" tries, timing out after 1.0 seconds"), failSpy.message)
         XCTAssertEqual(failSpy.file.hasSuffix("/ExpectToEventuallyEqualTests.swift"), true, "file")
         XCTAssertEqual(failSpy.line, 22, "line")
+    }
+
+    func test_message_forString() throws {
+        let message = messageEventuallyEqualFailed(expected: "abc", actual: "def", tryCount: 99, timeout: 0.1)
+        XCTAssertEqual(message, "Expected \"abc\", but was \"def\" after 99 tries, timing out after 0.1 seconds")
+    }
+
+    func test_message_forInt() throws {
+        let message = messageEventuallyEqualFailed(expected: 0, actual: 1, tryCount: 99, timeout: 0.1)
+        XCTAssertEqual(message, "Expected 0, but was 1 after 99 tries, timing out after 0.1 seconds")
     }
 }
 
