1821997139380ff7a8a6ed32d5a84abd60ede690
Jon Reid <jon@qualitycoding.org>
2023-12-21 18:28:12 -0800

    d - update file comment block

--- SampleApp/SampleApp/main.swift
+++ SampleApp/SampleApp/main.swift
@@ -1,4 +1,4 @@
-// ExpectToEventuallyEqual by Jon Reid, https://qualitycoding.org
+// ExpectToEventuallyEqual by Jon Reid (https://qualitycoding.org) and Steven Baker (https://stevenrbaker.com)
 // Copyright 2023 Jonathan M. Reid. https://github.com/jonreid/ExpectToEventuallyEqual/blob/main/LICENSE.txt
 // SPDX-License-Identifier: MIT
 
