a5c798d25cc226907dd68f124ce6a305cb162255
Jon Reid <jon@qualitycoding.org>
2025-03-28 22:02:46 -0700

    . d Add file header block to Swift Testing demo

--- SampleApp/SampleAppTests/TableViewControllerTests_SwiftTesting.swift
+++ SampleApp/SampleAppTests/TableViewControllerTests_SwiftTesting.swift
@@ -1,3 +1,7 @@
+// ExpectToEventuallyEqual by Jon Reid (https://qualitycoding.org) and Steven Baker (https://stevenrbaker.com)
+// Copyright 2023 Jonathan M. Reid. https://github.com/jonreid/ExpectToEventuallyEqual/blob/main/LICENSE.txt
+// SPDX-License-Identifier: MIT
+
 @testable import SampleApp
 import ExpectToEventuallyEqual
 import Testing
