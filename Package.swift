57675040bf13e15d1704af4889c6182839093315
Jon Reid <jon@qualitycoding.org>
2025-07-04 10:19:50 -0700

    . e Update dependency to FailKit 1.0.0

--- Package.swift
+++ Package.swift
@@ -5,10 +5,10 @@ import PackageDescription
 let package = Package(
     name: "ExpectToEventuallyEqual",
     platforms: [
-        .macOS(.v10_15),
-        .iOS(.v13),
-        .watchOS(.v6),
-        .tvOS(.v13)
+        .iOS(.v15),
+        .macOS(.v12),
+        .tvOS(.v15),
+        .watchOS(.v8),
     ],
     products: [
         .library(
@@ -16,7 +16,7 @@ let package = Package(
             targets: ["ExpectToEventuallyEqual"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/jonreid/FailKit.git", branch: "main")
+        .package(url: "https://github.com/jonreid/FailKit.git", branch: "1.0.0")
     ],
     targets: [
         .target(
